8446796209f2f846bef2feb26b6ca3325cc3d641
Rypo <ryan@gust.dev>
2023-04-04 04:04:45 -0500

    Create exception wrapped functions for non-essential html transforms

--- crawchet/utils/html.py
+++ crawchet/utils/html.py
@@ -1,4 +1,6 @@
+import css_inline
 import minify_html as _minify_html
+import lxml.html
 
 def html_minify(html_content:str, 
            do_not_minify_doctype=False,
@@ -59,3 +61,60 @@ def html_minify(html_content:str,
                     minify_js=minify_js, 
                     remove_bangs=remove_bangs,
                     remove_processing_instructions=remove_processing_instructions)
+
+
+def try_make_absolutelinks(html_content, base_url, decode=False, errors='ignore'):
+    if not isinstance(html_content, bytes):
+        html_content = html_content.encode()
+
+    if html_content == b'':
+        return html_content
+
+    try:
+        html_content = lxml.html.make_links_absolute(html_content, base_url=base_url)#, handle_failures='ignore')
+        #html_content = lxml.html.clean.autolink_html(html_content) # this breaks encoding for some reason
+    except Exception as e:
+        if errors == 'ignore':
+            pass
+        elif errors == 'print':
+            print(e)
+        elif errors == 'raise':
+            raise e
+        
+    if decode:
+        html_content = html_content.decode()#errors='ignore')
+
+    return html_content
+
+def try_inline_css(html_content, fetch_remote_css=False, remove_style_tags=True, errors='ignore'):
+    ''' Note: Only UTF-8 for string representation. Other document encodings are not yet supported.'''
+    try:
+        # Only UTF-8 for string representation. Other document encodings are not yet supported.
+        html_content = css_inline.inline(html_content, remove_style_tags=remove_style_tags, load_remote_stylesheets=fetch_remote_css) # base_url=url,
+    except Exception as e:
+        if fetch_remote_css:
+            try: 
+                html_content = css_inline.inline(html_content, remove_style_tags=remove_style_tags, load_remote_stylesheets=False)
+            except Exception as e:
+                if errors == 'ignore':
+                    pass
+                elif errors == 'print':
+                    print(e)
+                elif errors == 'raise':
+                    raise e
+                
+
+    return html_content
+
+def try_minify_html(html_content, errors='ignore', **kwargs):
+    try:
+        html_content = html_minify(html_content, **kwargs)
+    except Exception as e:
+        if errors == 'ignore':
+            pass
+        elif errors == 'print':
+            print(e)
+        elif errors == 'raise':
+            raise e
+
+    return html_content
