add955022c700da52a14d742139ceac13bfe459f
Rypo <ryan@gust.dev>
2023-04-04 03:48:21 -0500

    Add basic list io functions

--- crawchet/utils/io.py
+++ crawchet/utils/io.py
@@ -1,4 +1,26 @@
 from pathlib import Path
+from collections import OrderedDict
 
-def resolve_path(path):
-    return Path(path).resolve().as_posix()
+def resolve_path(path, as_str=True):
+    abspath = Path(path).resolve()
+    if as_str:
+        return abspath.as_posix()
+    return abspath
+
+
+def read_list(file_path, drop_duplicates=False):
+    with open(file_path,'r') as f:
+        url_list = f.read().splitlines()
+
+    if drop_duplicates:
+        return [*OrderedDict.fromkeys(url_list)]
+    
+    return url_list
+
+def write_list(item_list, file_path, drop_duplicates=False):
+    if drop_duplicates:
+        item_list = [*OrderedDict.fromkeys(item_list)]
+    
+    with open(file_path, 'w') as f:
+        f.writelines([item.strip()+'\n' for item in item_list])
+        
